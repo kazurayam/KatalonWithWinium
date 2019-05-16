5b7905d383e2cef3b9bb1d6745b6e14772b43346
kazuaki urayama <kazuaki.urayama@gmail.com>
2019-05-16 13:20:04 +0900

    add Drivers

---
+++ README.md
@@ -0,0 +1,13 @@
+Katalon with Winium
+========================
+
+Inspired by the following post at the Forum:
+- [Katalon with Winium for Desktop Applications](https://forum.katalon.com/t/katalon-with-winium-for-desktop-applications/12153)
+
+Additional information is available at Katalon Docs:
+- [Katalon with Winium for Desktop Applications](https://docs.katalon.com/katalon-studio/docs/katalon-with-winium-for-desktop-applications.html)
+
+https://github.com/2gis/Winium.Cruciatus
+
+
+https://docs.microsoft.com/ja-jp/windows/desktop/WinAuto/testing-tools
--- Scripts/TC1/Script1557956778384.groovy
+++ Scripts/TC1/Script1557956778384.groovy
@@ -1,16 +1,28 @@
-import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
-import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
-import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
-import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
-import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
-import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
-import com.kms.katalon.core.model.FailureHandling as FailureHandling
-import com.kms.katalon.core.testcase.TestCase as TestCase
-import com.kms.katalon.core.testdata.TestData as TestData
-import com.kms.katalon.core.testobject.TestObject as TestObject
-import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
-import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
-import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.By
+import org.openqa.selenium.WebElement
+import org.openqa.selenium.winium.DesktopOptions
+import org.openqa.selenium.winium.WiniumDriver
 
-String cat // comment
+WiniumDriver driver = null
+String appPath = "C:/Windows/System32/calc.exe"
+DesktopOptions option = new DesktopOptions()
+option.setApplicationPath(appPath)
+option.setDebugConnectToRunningApp(false)
+option.setLaunchDelay(2)
+driver = new WiniumDriver(new URL("http://localhost:9999"),option)
+Thread.sleep(1000)
+WebElement window = driver.findElementByClassName("CalcFrame")
+WebElement menuItem = window.findElement(By.id("MenuBar")).findElement(By.name("View"))
+menuItem.click()
+driver.findElementByName("Scientific").click()
+window.findElement(By.id("MenuBar")).findElement(By.name("View")).click()
+driver.findElementByName("History").click()
+window.findElement(By.id("MenuBar")).findElement(By.name("View")).click()
+driver.findElementByName("History").click()
+window.findElement(By.id("MenuBar")).findElement(By.name("View")).click()
+driver.findElementByName("Standard").click()
+driver.findElementByName("4").click()
+driver.findElementByName("Add").click()
+driver.findElementByName("5").click()
+driver.findElementByName("Equals").click()
+driver.close()
